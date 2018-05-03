b7c89aabb4d466123570cc4aef49129cc58e659e
Crasheth0 <37902710+Crasheth0@users.noreply.github.com>
2018-05-03 08:38:04 +0200

    Add files via upload

---
+++ dontworry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+def Dontworry ():
+   ans = raw_input ("Do you have a problem in your life? y o n\n ")
+   if ans == 'n' :
+        print ("Then Don't Worry!")
+   elif ans == 'y':
+            ans = raw_input("Can you Do Something about it? y o n\n")
+            if ans == 'n':
+                 print ("Then Don't Worry!")
+            else :
+                print ("Then Don't Worry!")
+Dontworry()
+                                            #Crasheth0
